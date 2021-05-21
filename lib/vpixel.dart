fe17bd9f608b60dca8ab44ede033860fe29e24ac
xiaobai <1063779828@qq.com>
2021-05-21 14:40:20 +0800

    fix: Sound and unsound null safety

--- lib/vpixel.dart
+++ lib/vpixel.dart
@@ -27,10 +27,10 @@ extension VPixelExp on num {
 
 //数据共享
 class _VPixelInherited extends InheritedWidget {
-  final MediaQueryData data;
+  final MediaQueryData? data;
   final Widget child;
-  final Key key;
-  const _VPixelInherited({this.key, this.data, this.child}) : super(key: key, child: child);
+  final Key? key;
+  const _VPixelInherited({this.key, this.data, required this.child}) : super(key: key, child: child);
   @override
   bool updateShouldNotify(covariant _VPixelInherited oldWidget) {
     return data != oldWidget.data;
@@ -39,17 +39,17 @@ class _VPixelInherited extends InheritedWidget {
 
 //虚拟像素小部件
 class VPixel extends StatefulWidget {
-  final WidgetBuilder builder;
-  final Key key;
+  final WidgetBuilder? builder;
+  final Key? key;
   final double designWidth;
-  const VPixel({this.key, this.builder, this.designWidth}) : assert(designWidth != null), super(key: key);
+  const VPixel({this.key, this.builder, required this.designWidth}) : assert(designWidth != null), super(key: key);
   @override
   State<StatefulWidget> createState() {
     return _VPixelState();
   }
 
   //获取UI尺寸数据
-  static MediaQueryData of(BuildContext ctx) {
+  static MediaQueryData? of(BuildContext ctx) {
     return ctx.dependOnInheritedWidgetOfExactType<_VPixelInherited>()?.data;
   }
 }
@@ -60,32 +60,32 @@ class _VPixelState extends State<VPixel> with WidgetsBindingObserver {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addObserver(this);
+    WidgetsBinding.instance!.addObserver(this);
   }
 
   @override
   void dispose() {
     super.dispose();
-    WidgetsBinding.instance.removeObserver(this);
+    WidgetsBinding.instance!.removeObserver(this);
   }
 
   @override
   void didChangeMetrics() {
     //UI尺寸发生变化时，刷新状态
     setState(() {
-      RendererBinding.instance.scheduleWarmUpFrame();
+      RendererBinding.instance!.scheduleWarmUpFrame();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    final data = MediaQueryData.fromWindow(WidgetsBinding.instance?.window);
+    final data = MediaQueryData.fromWindow(WidgetsBinding.instance!.window);
     _initVPixel(data, widget.designWidth);
     return _VPixelInherited(
       data: data,
       child: Builder(
         builder: (ctx) {
-          return widget.builder(ctx);
+          return widget.builder!(ctx);
         },
       ),
     );
